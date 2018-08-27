44ca39c2a2df4d699f33a05b988e176d490d9ea0
Cordrear <mad_tag@mail.ru>
2018-08-27 21:05:16 +0300

    Добавлено отображение загрузки постера рекомендованных фильмов

--- src/components/Recommendation/index.js
+++ src/components/Recommendation/index.js
@@ -5,6 +5,7 @@ import API from '../../helpers/API';
 const Recommendation = (props) => {
 	return(
 		<article className='recommended-movie'>
+			<p className='loading'>Загрузка...</p>
 			<img 
 				src={API.movies.getPosterPath(props.movie.poster_path)}
 				alt={props.movie.title}
