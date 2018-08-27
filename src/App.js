d4584454610c08897e774c85f337ea2ac9666f96
Cordrear <mad_tag@mail.ru>
2018-08-27 10:47:49 +0300

    js-файл MovieDBApp переименован в App.js и вынесен на уровень выше

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import API from '../helpers/API';
-import MyLocalStorage from '../helpers/localStorage';
-import Pagination from '../components/Pagination';
-import MovieList from '../components/MovieList';
-import Header from '../components/Header';
-import Underlay from '../components/Underlay';
-import SingleMovie from '../components/SingleMovie';
+import API from './helpers/API';
+import MyLocalStorage from './helpers/localStorage';
+import Pagination from './components/Pagination';
+import MovieList from './components/MovieList';
+import Header from './components/Header';
+import Underlay from './components/Underlay';
+import SingleMovie from './components/SingleMovie';
 
 class MovieDBApp extends React.Component {
 	constructor(props) {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
-import MovieDBApp from './MovieDBApp';
+import MovieDBApp from './App.js';
 
 import './style.less';
 
